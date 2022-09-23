9ec512d4131ccd487ad98fa8a74694043024822d
sungbum05 <sblim05@naver.com>
2022-09-23 16:30:32 +0900

    ~2022-09-23(4:30)_BackJoon

---
+++ CSharp_Study/Silver4_BackJoon9012_X/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silver4_BackJoon9012_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int cnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cnt; i++)
+            {
+                string input = Console.ReadLine();
+                bool Check = true;
+
+                foreach (var item in input)
+                {
+                    switch(item)
+                    {
+                        case '(':
+                            stack.Push(i);
+                            break;
+
+                        case ')':
+                            if(stack.Count <= 0)
+                            {
+                                Check = false;
+                                break;
+                            }
+
+                            else
+                                stack.Pop();
+                            break;
+                    }
+                }
+
+                if (Check == false || stack.Count > 0)
+                {
+                    Console.WriteLine("NO");
+                    stack.Clear();
+                }
+
+                else
+                    Console.WriteLine("YES");
+            }
+        }
+    }
+}
---
+++ CSharp_Study/Silver5_BackJoon11866_X/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silver5_BackJoon11866_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            Queue<int> queue2 = new Queue<int>();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            int cnt = 0;
+
+            //값 설정
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            for (int i = 1; i <= input[0]; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            stringBuilder.Append("<");
+
+            while(queue.Count > 0 || queue2.Count > 0)
+            {
+                if(queue.Count == 0 && queue2.Count > 0)
+                    queue = queue2;
+
+                cnt++;
+
+                if(cnt % input[1] == 0)
+                {
+                    stringBuilder.Append($"{queue.Dequeue()}, ");
+                }
+
+                else
+                {
+                    queue2.Enqueue(queue.Dequeue());
+
+                    if(queue.Count <= 0)
+                    {
+                        queue = queue2;
+                    }
+                }
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append(">");
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
